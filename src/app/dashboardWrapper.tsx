d9cc775b12bd647580bb4c18843ad60f94e11c1a
Ali Fahed <alifahed67@gmail.com>
2024-11-22 11:55:03 +0800

    Add Responsive Sidebar & Dark Mode

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,11 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
+
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useSelector(
+    (state: RootState) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useSelector((state: RootState) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-      <main className="dark:bg-dark-bg flex w-full flex-col bg-gray-50">
+      <main
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 dark:bg-gray-700 ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        } `}
+      >
         <Navbar />
         {children}
       </main>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,34 @@
 "use client";
 
+import { setIsSidebarCollapsed } from "@/store";
+import { RootState } from "@/store/store";
+import {
+  Briefcase,
+  Home,
+  LucideIcon,
+  Search,
+  Settings,
+  User,
+  Users,
+  X,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const dispatch = useDispatch();
+  const isSidebarCollapsed = useSelector(
+    (state: RootState) => state.global.isSidebarCollapsed,
+  );
+
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
   const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
-  transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
+  transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
+  transform ${isSidebarCollapsed ? "-translate-x-full" : "translate-x-0"} w-64`;
 
   return (
     <div className={sidebarClassNames}>
@@ -17,7 +38,16 @@ const Sidebar = () => {
           <div className="text-xl font-bold text-gray-800 dark:text-white">
             Projman
           </div>
-          X
+          {isSidebarCollapsed ? null : (
+            <button
+              className="py-3"
+              onClick={() =>
+                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+              }
+            >
+              <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+            </button>
+          )}
         </div>
 
         {/* Team */}
@@ -28,8 +58,47 @@ const Sidebar = () => {
         </div>
 
         {/* Navbar Links */}
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+          <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
+          <SidebarLink icon={Search} label="Search" href="/search" />
+          <SidebarLink icon={Settings} label="Settings" href="/settings" />
+          <SidebarLink icon={User} label="Users" href="/users" />
+          <SidebarLink icon={Users} label="Teams" href="/teams" />
+        </nav>
       </div>
     </div>
+  );
+};
+
+interface SidebarLinkTypes {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+}
+
+const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkTypes) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
+          isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+        } justify-start px-8 py-3`}
+      >
+        {isActive && (
+          <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
+        )}
+
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
 
